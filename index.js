1a59f348af7be3c353b36bbd2274c1cb7e449573
Krzysztof Apolinarski <k.apolinarski@gmail.com>
2018-12-08 18:06:36 +0100

    CORS

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const textRouter = require('./textRouter');
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 app.get('/', (request, response) => {
     response.send('Welcome to my virtual board');
 });
