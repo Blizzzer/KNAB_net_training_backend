a9efabf7507bdaaced47cab6336ab7507bc04910
Krzysztof Apolinarski <k.apolinarski@gmail.com>
2018-12-08 18:12:28 +0100

    Changed db account

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ exports.DEBUG = true;
 exports.DB_CONFIG = {
     host: "localhost",
     port: "3306",
-    user: "root",
-    password: "slash123",
+    user: "szkolenie",
+    password: "knabjestthebest",
     database: "virtual_board"
 };
