18757e560e6b7aa571a8d06b3f64faa4faa606aa
Krzysztof Apolinarski <k.apolinarski@gmail.com>
2018-12-12 20:17:12 +0100

    CORS fix for post

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
     next();
 });
 
